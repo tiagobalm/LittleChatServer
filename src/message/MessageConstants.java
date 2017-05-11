3d0b2fe86a55f1c4aacfe04a1b81ad93b80c5a8b
VascoUP <vasco_mag_pereira@hotmail.com>
2017-05-11 16:27:07 +0100

    Message consts has package private

--- src/message/MessageConstants.java
+++ src/message/MessageConstants.java
@@ -1,41 +1,24 @@
 package message;
 
-public class MessageConstants {
-    public static final String loginType = "LOGIN";
-    public static final int loginSize= 5;
+class MessageConstants {
+    static final String loginType = "LOGIN";
+    static final int loginSize= 5;
 
-    public static final String logoutType = "LOGOUT";
-    public static final int logoutSize= 1;
+    static final String logoutType = "LOGOUT";
+    static final int logoutSize= 1;
 
-    public static final String registerType = "REGISTER";
-    public static final int registerSize= 5;
+    static final String registerType = "REGISTER";
+    static final int registerSize= 5;
 
-    public static final String getRoomsType = "GETROOMS";
-    public static final int getRoomsSize = 1;
+    static final String getRoomsType = "GETROOMS";
+    static final int getRoomsSize = 1;
 
-    public static final String getFriendsType = "GETFRIENDS";
-    public static final int getFriendsSize = 1;
+    static final String getFriendsType = "GETFRIENDS";
+    static final int getFriendsSize = 1;
 
-    public static final String getFriendRequestsType = "GETFRIENDREQUESTS";
-    public static final int getFriendRequestsSize = 1;
+    static final String getFriendRequestsType = "GETFRIENDREQUESTS";
+    static final int getFriendRequestsSize = 1;
 
-    public static final String getMessagesType = "GETMESSAGES";
-    public static final int getMessagesSize = 2;
-
-    /*
-    public static final String messageType = "MESSAGE";
-    public static final int messageSize= 3;
-
-    public static final String friendRequestType = "FRIENDREQUEST";
-    public static final int friendRequestSize= 3;
-
-    public static final String friendRefuseType = "FRIENDREFUSE";
-    public static final int friendRefuseSize= 3;
-
-    public static final String friendAcceptType = "FRIENDACCEPT";
-    public static final int friendAcceptSize= 3;
-
-    public static final String requestIPType = "REQUESTIP";
-    public static final int requestIPSize= 3;
-    */
+    static final String getMessagesType = "GETMESSAGES";
+    static final int getMessagesSize = 2;
 }
