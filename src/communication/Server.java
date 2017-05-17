7d12b7f00f19a9fa16eeb24649f6c1b036942d58
saracouto1318 <saracouto1318@hotmail.com>
2017-05-17 16:28:09 +0100

    communication package commented

--- src/communication/Server.java
+++ src/communication/Server.java
@@ -16,39 +16,91 @@ import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * Server's main class
+ */
 public class Server {
+    /**
+     * Keystore path's string
+     */
     private static final String keystorePath = Server.class.getResource("../keys/server.private").getPath();
+    /**
+     * Keystore pass
+     */
     private static final String keystorePass = "littlechat";
+    /**
+     * Truststore path's name
+     */
     private static final String truststorePath = Server.class.getResource("../keys/truststore").getPath();
+    /**
+     * Truststore pass
+     */
     private static final String truststorePass = "littlechat";
 
+    /**
+     * Server's main port
+     */
     private static final int MAIN_PORT = 15000;
+    /**
+     * Backup protocol's port
+     */
     private static final int BACKUP_PORT = 14999;
+    /**
+     * Worker threads' number
+     */
     private static final int numberOfWorkerThreads = 20;
-
+    /**
+     * Instance of server
+     */
     private static Server ourInstance;
 
+    /**
+     * Socket that will be used in this server
+     */
     private SSLServerSocket sslserversocket;
+    /**
+     * Backup protocol's channel
+     */
     private BackUpConnection backupChannel;
 
+    /**
+     * Known clients that are saved in this server
+     */
     private Map<Integer, ClientConnection> knownClients;
+    /**
+     * Messages saved in this server
+     */
     private BlockingQueue<Map.Entry<ClientConnection, Message>> messages;
 
+    /**
+     * Variable that indicates if the server is backed up or not
+     */
     private final boolean isBackUpServer;
 
+    /**
+     * Server's constructor
+     * @param isBackUpServer Variable that indicates if the server is backed up pr not
+     */
     private Server(boolean isBackUpServer) {
         this.isBackUpServer = isBackUpServer;
         knownClients = new HashMap<>();
         messages = new LinkedBlockingQueue<>();
     }
 
+    /**
+     * This function creates the server
+     * @param isBackUpServer This variable indicates if a server is backed up or not
+     * @throws Exception This exception is thrown if the server initiates a second instance
+     */
     private static void createServer(boolean isBackUpServer) throws Exception {
         if( ourInstance != null )
             throw new Exception("Singleton class Server initiated twice");
         ourInstance = new Server(isBackUpServer);
     }
 
-
+    /**
+     * Initializes the server
+     */
     private void initialize() {
         setSystemSettings();
         //startBackUpConnection();
@@ -59,6 +111,9 @@ public class Server {
         startAcceptThread();
     }
 
+    /**
+     * Starts the backup protocol's connection
+     */
     private void startBackUpConnection() {
         try {
             if( isBackUpServer )
@@ -72,6 +127,10 @@ public class Server {
         backupChannel = BackUpConnection.getInstance();
     }
 
+    /**
+     * Starts the server with the respective port
+     * @param port Port that will used to create the socket
+     */
     private void startServer(int port) {
         System.out.println("Starting server sockets");
 
@@ -94,6 +153,9 @@ public class Server {
         sslserversocket.setEnabledCipherSuites(ciphers);
     }
 
+    /**
+     * Starts the acceptation of threads
+     */
     private void startAcceptThread() {
         System.out.println("Starting accept thread");
         Thread accept = new Thread(() -> {
@@ -111,6 +173,9 @@ public class Server {
         accept.start();
     }
 
+    /**
+     * Starts the worker threads
+     */
     private void startWorkerThreads() {
         System.out.println("Starting worker threads");
         ExecutorService executor = Executors.newFixedThreadPool(numberOfWorkerThreads);
@@ -120,6 +185,9 @@ public class Server {
         }
     }
 
+    /**
+     * Sets the system's main settings
+     */
     private void setSystemSettings() {
         System.setProperty("javax.net.ssl.keyStore", keystorePath);
         System.setProperty("javax.net.ssl.keyStorePassword", keystorePass);
@@ -127,34 +195,57 @@ public class Server {
         System.setProperty("javax.net.ssl.trustStorePassword", truststorePass);
     }
 
-
+    /**
+     * Gets the server's instance
+     * @return The server's instance
+     */
     @Contract(pure = true)
     public static Server getOurInstance() {
         return ourInstance;
     }
 
+    /**
+     * Gets the messages saved in the server
+     * @return The messages saved in the server
+     */
     public BlockingQueue<Map.Entry<ClientConnection, Message>> getMessages() {
         return messages;
     }
 
-
+    /**
+     * Gets the client's connection using is identifier
+     * @param id The client's identifier
+     * @return The client's connection
+     */
     @Nullable
     public ClientConnection getClientByID(Integer id) {
         return knownClients.get(id);
     }
 
+    /**
+     * Adds a client by id, adding also its connection
+     * @param id The client's identifier
+     * @param client The client's connection
+     */
     public void addClientID(Integer id, ClientConnection client) {
         client.setClientID(id);
         knownClients.put(id, client);
     }
 
+    /**
+     * Removes a client using its identifier
+     * @param id The client's identifier
+     */
     public void removeByID(Integer id) {
         ClientConnection c = knownClients.get(id);
         knownClients.remove(id);
         c.setClientID(null);
     }
 
-
+    /**
+     * Server's main function
+     * @param args Arguments used in the server's main function
+     */
     public static void main(String[] args) {
         if( args.length != 1 ) return;
         boolean isBackUpServer = Objects.equals("true", args[0]);
--- src/communication/StreamMessage.java
+++ src/communication/StreamMessage.java
@@ -7,12 +7,27 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class builds a streamed message
+ */
 public class StreamMessage {
+    /**
+     * SSL socket to be read/write the streamed message
+     */
     private SSLSocket sslSocket;
+    /**
+     *  Input stream
+     */
     private ObjectInputStream is;
+    /**
+     * Output stream
+      */
     private ObjectOutputStream os;
 
-
+    /**
+     * StreamMessage's constructor
+     * @param sslSocket SSL socket
+     */
     public StreamMessage(SSLSocket sslSocket) {
         this.sslSocket = sslSocket;
         try {
@@ -25,6 +40,9 @@ public class StreamMessage {
         }
     }
 
+    /**
+     * Closes the input and output streams and the SSL socket
+     */
     public void close() {
         try {
             is.close();
@@ -35,10 +53,21 @@ public class StreamMessage {
         }
     }
 
+    /**
+     * Redas the message
+     * @return The message read
+     * @throws IOException Signals that an I/O exception of some sort has occurred. This class is the general class of exceptions produced by failed or interrupted I/O operations
+     * @throws ClassNotFoundException Thrown when an application tries to load in a class through its string name, but no definition for the class with the specified name could be found
+     */
     public synchronized Message read() throws IOException, ClassNotFoundException {
         return (Message) is.readObject();
     }
 
+    /**
+     * Writes the message
+     * @param message Message that will be written
+     * @throws IOException Signals that an I/O exception of some sort has occurred. This class is the general class of exceptions produced by failed or interrupted I/O operations
+     */
     public void write(Message message) throws IOException {
         synchronized (os) {
             os.writeObject(message);
